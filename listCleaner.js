e0bfbaaec9da84ddd8a207aa825b3edc06c308e9
Legendword <zwh@legendword.com>
2019-01-04 19:30:17 -0800

    debugged algorithm, first raw dataset parsed

--- listCleaner.js
+++ listCleaner.js
@@ -1,10 +1,14 @@
 /*
  *     Author: legendword
  *     Created:   2019/01/04 18:29:02.524204
- *     Last Modified: 2019/01/04 18:44:44.381490
+ *     Last Modified: 2019/01/04 19:28:36.898422
  *     Notes (if any): 
  */
 var fs = require('fs');
+var csv = {
+	stringify: require('csv-stringify'),
+	parse: require('csv-parse')
+};
 
 fs.readFile('original.txt', 'utf8', (err, data) => {
 	if (err) throw err;
@@ -12,18 +16,21 @@ fs.readFile('original.txt', 'utf8', (err, data) => {
 	var i=-1;
 	var inp = false;
 	var parsed = new Array(""),pi = 0;
+	//console.log(data.length);
 	while (i+1<data.length){
 		i++;
-		if (!isNaN(parseInt(data[i]))){
-			continue;
-		}
-		if (inp) continue;
+		//console.log(i,data[i],inp);
+		
 		if (data[i]=='（'){
 			inp = true;
 			continue;
 		}
 		else if (data[i]=='）'){
 			inp = false;
+			continue;
+		}
+		else if (inp) continue;
+		else if (!isNaN(parseInt(data[i]))){
 			continue;
 		}
 		else if (data[i]=='*'||data[i]=='+') continue;
@@ -37,15 +44,16 @@ fs.readFile('original.txt', 'utf8', (err, data) => {
 			parsed[pi] += data[i];
 		}
 	}
+	//console.log(parsed);
 	var cities = new Array();
 	var output = "";
 	parsed.forEach((j,i)=>{
 		if (j.indexOf("学")==-1){
-			cities.push(j);
+			if (cities.indexOf(j)==-1) cities.push(j);
 		}
 		else output += j+",";
 	});
-	console.log("schools = [",output,"];");
-	console.log("cities = [",cities.join(','),"];");
+	console.log(output.slice(0,output.length-1));
+	console.log(cities.join(','));
 	//console.log(parsed);
 });
